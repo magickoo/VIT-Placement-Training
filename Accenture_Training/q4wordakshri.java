5473b6d8ea4c06410c06f17b0f808a3934ce6501
Khushi Agarwal <magickhu@gmail.com>
2024-03-06 20:19:50 +0530

    Update q4wordakshri.java

--- Accenture_Training/q4wordakshri.java
+++ Accenture_Training/q4wordakshri.java
@@ -1,1 +1,20 @@
 
+import java.util.*;
+public class q4wordakshri {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String a,b="";
+        String c="####";
+        do{
+            a=sc.nextLine();
+            if(b.isEmpty()||a.charAt(0)==b.charAt(b.length()-1)){
+                System.out.println(a);
+            }
+            b = a;
+        }while(!a.equals(c));
+        sc.close();
+        
+
+    }
+    
+}
