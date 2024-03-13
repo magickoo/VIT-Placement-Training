1e08c2531dc4b2eb2c004757ff21492c70db0744
Khushi Agarwal <magickhu@gmail.com>
2024-03-13 09:50:25 +0530

    Create NQueen.java

---
+++ SDOT_Training/NQueen.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main
+{
+    public static void fun(int n){
+        if(n==1){
+            System.out.println(n);
+        }
+        else{
+            fun(n-1);
+            System.out.printlan(n);
+            fun(n-1);
+        }
+    }
+	public static void main(String[] args) {
+		fun(4);
+		//.out.println();
+	}
+}
